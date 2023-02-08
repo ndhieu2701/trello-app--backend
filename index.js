520059e68a0837a37201c4da6a6e43140e731f65
ndhieu2701 <hieu@v7rx3.onmicrosoft.com>
2023-02-08 15:26:45 +0700

    update code to deploy

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const app = express()
-require('dotenv').config();
-
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const compression = require("compression");
+require("dotenv").config();
 // //app use cors
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-app.use(express.urlencoded({extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+process.env.NODE_ENV = "production";
+
+// Use morgan for logging
+app.use(morgan("combined"));
+
+// Enable response compression
+app.use(compression());
 //connect database
-const db = require('./config/db')
-db.connect()
+const db = require("./config/db");
+db.connect();
 
-const route = require('./api/index')
-route(app)
+const route = require("./api/index");
+route(app);
 
-app.listen(process.env.PORT, () => {
-    console.log(`app listen on port ${process.env.PORT}`);
-})
-
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`app listen on port ${PORT}`);
+});
